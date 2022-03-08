32707521b45dace3afc06e0f1a3adbf81499b5d5
Chris Guizzetti <guizzettic@gmail.com>
2022-03-08 09:00:08 -0700

    pulling rover information from API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({});
 
 function App() {
+  const url =
+    'https://api.nasa.gov/mars-photos/api/v1/rovers/?api_key=myVdUkv9z8xAeDgHj0CNCxTfO1BzYYMV8bMNklQc';
+  const [roversList, setRoversList] = useState(null);
+  const classes = useStyles();
+
+  useEffect(() => {
+    const fetchRovers = () => {
+      fetch(url)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(`Request failed with status ${res.status}`);
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setRoversList(data.rovers);
+        })
+        .catch((error) => console.log(error));
+    };
+
+    fetchRovers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {roversList &&
+        roversList.map((r) => (
+          <div
+            style={{
+              border: '2px solid grey',
+              width: 200,
+              margin: 5,
+              padding: 10,
+            }}
+          >
+            {r.id}
+            {r.name}
+          </div>
+        ))}
     </div>
   );
 }
