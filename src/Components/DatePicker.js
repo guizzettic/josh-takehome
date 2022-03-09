58206af99396d13c72e092a918259c22471ee951
Chris Guizzetti <guizzettic@gmail.com>
2022-03-09 13:23:48 -0700

    updated datepicker to have calendar icon

--- src/Components/DatePicker.js
+++ src/Components/DatePicker.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import DateMomentUtils from '@date-io/moment';
-import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 const DatePick = ({ date, setSearchDate }) => {
   const handleDateChange = (newDate) => {
@@ -11,8 +11,12 @@ const DatePick = ({ date, setSearchDate }) => {
 
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <DatePicker value={date} onChange={handleDateChange} />
-      <CalendarMonthIcon />
+      <KeyboardDatePicker
+        style={{ width: 150 }}
+        value={date}
+        onChange={handleDateChange}
+        format="MM/dd/yyyy"
+      />
     </MuiPickersUtilsProvider>
   );
 };
